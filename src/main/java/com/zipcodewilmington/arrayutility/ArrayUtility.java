f3be75cef0fd2b2dc9f83b2f77ef903fbaf8184d
April Rivera <aprilrivera@zipcoders-MBP-2.fios-router.home>
2018-03-07 20:07:18 -0500

    Done duplicates

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,46 @@
 package com.zipcodewilmington.arrayutility;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    ArrayList<T> inputList = new ArrayList<>();
+
+    public ArrayUtility(T[] inputArray) {
+        for(T t : inputArray){
+            inputList.add(t);
+        }
+    }
+
+    public <E> E countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
+        for(T e : arrayToMerge){
+            inputList.add(e);
+        }
+
+        Integer counter = 0;
+        for(T t : inputList){
+            if(t.equals(valueToEvaluate)){
+                counter++;
+                continue;
+            }
+
+        }return (E) counter;
+    }
+
+    public T[] removeValue(T valueToRemove){
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge){
+        return null;
+    }
+
+    public T getNumberOfOccurrences(T valueToEvaluate){
+        return null;
+    }
 }
