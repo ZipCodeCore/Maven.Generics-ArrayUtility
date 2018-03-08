8c2ba2a87143e4163efc699fb44890c02190c441
April Rivera <aprilrivera@zipcoders-MBP-2.fios-router.home>
2018-03-07 22:16:05 -0500

    casting issues with remove value, everything else passes

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,46 +1,70 @@
 package com.zipcodewilmington.arrayutility;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T> {
 
-    ArrayList<T> inputList = new ArrayList<>();
+   private ArrayList<T> inputList = new ArrayList<>();
 
     public ArrayUtility(T[] inputArray) {
-        for(T t : inputArray){
-            inputList.add(t);
-        }
+        inputList.addAll(Arrays.asList(inputArray));
     }
 
-    public <E> E countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
-        for(T e : arrayToMerge){
-            inputList.add(e);
-        }
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
+        inputList.addAll(Arrays.asList(arrayToMerge));
 
         Integer counter = 0;
         for(T t : inputList){
             if(t.equals(valueToEvaluate)){
                 counter++;
-                continue;
             }
 
-        }return (E) counter;
+        }return counter;
     }
 
     public T[] removeValue(T valueToRemove){
-        return null;
+        inputList.removeAll(Arrays.asList(valueToRemove));
+        return (T[]) inputList.toArray();
     }
 
-    public T getMostCommonFromMerge(T[] arrayToMerge){
-        return null;
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        inputList.addAll(Arrays.asList(arrayToMerge));
+
+        Map<T, Integer> inputMap = new HashMap<>(inputList.size());
+        int maxCount = Integer.MIN_VALUE;
+        T mostCommon = null;
+        for (T t : inputList) {
+            if (!inputMap.containsKey(t)) {
+                inputMap.put(t, 1);
+            } else {
+                inputMap.put(t, inputMap.get(t) + 1);
+            }
+            maxCount = 0;
+            for (Map.Entry<T, Integer> entry : inputMap.entrySet()) {
+                if (entry.getValue() > maxCount) {
+                    maxCount = entry.getValue();
+                    mostCommon = entry.getKey();
+                }
+            }
+        }
+        return mostCommon;
     }
 
-    public T getNumberOfOccurrences(T valueToEvaluate){
-        return null;
+    public Integer getNumberOfOccurrences(T valueToEvaluate){
+        Integer counter = 0;
+        for(T t : inputList){
+            if(t == null){
+                counter = 1;
+            }else if(t.equals(valueToEvaluate)){
+                counter++;
+            }
+        }return counter;
     }
 }
