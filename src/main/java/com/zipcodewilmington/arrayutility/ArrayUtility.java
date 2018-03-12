1e0435a6c88beb5cc6afcf23cd92b3212ef8ee44
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-12 17:27:17 -0400

    finished first ArrayUtility

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,42 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<E> {
+
+    private E[] inputArray;
+
+    public ArrayUtility(E[] inputArray) {
+        this.inputArray = inputArray;
+    }
+
+    public int countDuplicatesInMerge(E[] arrayToMerge, E valueToEvaluate) {
+        int count = 0;
+        E[] finalArray = mergeArrays(arrayToMerge);
+        for (E element : finalArray) {
+            if (element.equals(valueToEvaluate)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public E[] mergeArrays(E[] arrayToMerge) {
+        ArrayList<E> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(inputArray));
+        arrayList.addAll(Arrays.asList(arrayToMerge));
+        return (E[]) arrayList.toArray();
+    }
+
+    public int getNumberOfOccurrences(E valueToEvaluate) {
+        return 0;
+    }
+
+
+
+
 }
